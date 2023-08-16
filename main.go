5b0c6b21cb6b61c9d4cec068f1f48587834eee67
Matthew Slauson <matthew@slauson.io>
2023-08-15 23:15:23 -0500

    switch to gorilla

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"text/template"
-	"time"
+
+	"github.com/gorilla/mux"
 )
 
 // Create a struct that holds information to be displayed in our HTML file
@@ -13,37 +13,26 @@ type Welcome struct {
 	Time string
 }
 
-// Go application entrypoint
+var templates *template.Template
+
 func main() {
-	// Instantiate a Welcome struct object and pass in some random information.
-	// We shall get the name of the user as a query parameter from the URL
-	welcome := Welcome{"Anonymous", time.Now().Format(time.Stamp)}
+	// Parse your templates
+	templates = template.Must(template.ParseGlob("web/template/*.tmpl"))
 
-	// We tell Go exactly where we can find our html file. We ask Go to parse the html file (Notice
-	// the relative path). We wrap it in a call to template.Must() which handles any errors and halts if there are fatal errors
+	r := mux.NewRouter()
+	r.HandleFunc("/", HomeHandler)
+	http.Handle("/", r)
 
-	templates := template.Must(template.ParseGlob("web/template/*.tmpl"))
-	templates = template.Must(templates.ParseGlob("web/template/pages/*.tmpl"))
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
+}
 
-	// Our HTML comes with CSS that go needs to provide when we run the app. Here we tell go to create
-	// a handle that looks in the static directory, go then uses the "/static/" as a url that our
-	// html can refer to when looking for our css and other files.
-
-	http.Handle(
-		"/static/",
-		http.StripPrefix("/static/", http.FileServer(http.Dir("static"))),
-	) // Go looks in the relative "static" directory first using http.FileServer(), then matches it to a
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if name := r.FormValue("name"); name != "" {
-			welcome.Name = name
-		}
-
-		if err := templates.ExecuteTemplate(w, "base", welcome); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-	})
-
-	fmt.Println("Listening")
-	fmt.Println(http.ListenAndServe(":8080", nil))
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	data := map[string]interface{}{} // Fill in data as required
+	err := templates.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
